7179fc88aee1cf6d8d2d20be9668b83120a5ba02
m5x5 <michaelpeters5647@gmail.com>
2022-06-27 17:04:08 +0200

    feat: :sparkles: Bilder mit Interaktionen hinzufügen

--- includes/content/Startseite.php
+++ includes/content/Startseite.php
@@ -4,6 +4,6 @@
       <img src="includes/img/home/UK_HERZ.png" alt="Image" height="#" height="90" width="90" class="herz" />
       lich Willkommen!
     </h2>
-    <img src="includes/img/home/opening_page.jpg" alt="Image" height="600" width="700" />
+    <img src="includes/img/home/opening_page.jpg" alt="Image" class="cover" />
   </div>
 </article>

33599b91fa1191c296fdf21552cee5fb7b59d36b
m5x5 <michaelpeters5647@gmail.com>
2022-06-27 17:19:40 +0200

    style: :recycle: Centralize CSS colors

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,12 @@
 <?PHP
 ?>
 
-<a href="#" id="sprung-marke">&#129044; Zurück</a>
+<a href="#" id="sprung-marke">
+  <svg xmlns="http://www.w3.org/2000/svg" class="text-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M11 17l-5-5m0 0l5-5m-5 5h12" />
+  </svg>
+  Zurück
+</a>
 
 <h6>
   &copy; <a href="https://www.maxedvpro.de/" target="_blank">Max EDV Pro</a>

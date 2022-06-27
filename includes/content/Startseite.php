2031283bf271687da59291b6c8f218d447c391bd
m5x5 <michaelpeters5647@gmail.com>
2022-06-27 17:25:52 +0200

    fix: :bug: Cover auf der Startseite begrenzen

--- includes/content/Startseite.php
+++ includes/content/Startseite.php
@@ -1,9 +1,7 @@
 <article class="card" id="startseite">
-  <div class="textrahmen">
-    <h2>
-      <img src="includes/img/home/UK_HERZ.png" alt="Image" height="#" height="90" width="90" class="herz" />
-      lich Willkommen!
-    </h2>
-    <img src="includes/img/home/opening_page.jpg" alt="Image" class="cover" />
-  </div>
+  <h2>
+    <img src="includes/img/home/UK_HERZ.png" alt="Image" height="#" height="90" width="90" class="herz" />
+    lich Willkommen!
+  </h2>
+  <img src="includes/img/home/opening_page.jpg" alt="Image" class="cover" />
 </article>
